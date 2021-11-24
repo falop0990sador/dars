5fb0d93fa44fbf0664c69ad5ea5c9b75257de6cc
TechLine <galopi0987s@email.com>
2021-11-24 19:50:59 +0600

    Initial commit

---
+++ src/com/company/Bank.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Bank {
+    private String password;
+    private int amount;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
